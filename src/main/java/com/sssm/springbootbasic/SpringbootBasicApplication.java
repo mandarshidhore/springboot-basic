c40bb2adc4fda5f2ac7ec764806774943c9460ed
Mandar.Shidhore <mandar.shidhore@travelport.com>
2018-09-30 06:51:51 -0600

    updated comment

--- src/main/java/com/sssm/springbootbasic/SpringbootBasicApplication.java
+++ src/main/java/com/sssm/springbootbasic/SpringbootBasicApplication.java
@@ -6,14 +6,14 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import com.sssm.springbootbasic.model.ProgressTracker;
 
-// @SpringBootApplication annotation indicates to Spring container - this is a stating point for Spring Boot Application.
+// @SpringBootApplication indicates application stating point to Spring container.
 @SpringBootApplication
 public class SpringbootBasicApplication {
 
 	public static void main(String[] args) {
 		System.out.println("## Running SpringbootBasicApplication ##");
 		ConfigurableApplicationContext context = SpringApplication.run(SpringbootBasicApplication.class, args);
-		// above line does following things -
+		// SpringApplication.run(...) does following things -
 		// 1. Sets up default configuration.
 		// 2. Starts Spring application context.
 		// 3. Performs class path scan.
